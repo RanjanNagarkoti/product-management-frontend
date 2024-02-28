26a3e8323e0523efe7fe74991b2db9a1204fe2eb
Ranjan Nagarkoti <ranjan.ngr2058@gmail.com>
2024-02-28 22:36:10 +0545

    feat: category

---
+++ src/components/category/Category.jsx
@@ -0,0 +1,63 @@
+import React, { useState } from "react";
+import { useContext, useEffect } from "react";
+import { AuthContext } from "../../context/AuthContext.jsx";
+import { useMutation, QueryClient } from "react-query";
+import client from "../..//network/api.js";
+import { FaTrashAlt } from "react-icons/fa";
+import { LuFileEdit } from "react-icons/lu";
+
+const Category = ({ category }) => {
+  const queryClient = new QueryClient();
+
+  const { token } = useContext(AuthContext);
+
+  const deleteCategory = useMutation(
+    (id) =>
+      client.delete(`/categories/${id}`, {
+        headers: {
+          Authorization: token,
+        },
+      }),
+    {
+      onSuccess: () => {
+        queryClient.invalidateQueries("categories");
+      },
+    }
+  );
+
+  return (
+    <tr className="bg-white dark:bg-gray-800">
+      <th
+        scope="row"
+        className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+      >
+        {category.id}
+      </th>
+      <td className="px-6 py-4 font-normal text-gray-900 whitespace-nowrap dark:text-white">
+        {category.title}
+      </td>
+      <td className="px-6 py-4 font-normal text-gray-900 whitespace-nowrap dark:text-white">
+        {category.slug}
+      </td>
+      <td className="px-6 py-4 font-normal text-gray-900 whitespace-nowrap dark:text-white">
+        <div className="flex">
+          <button className="text-blue-700 mr-1" title="Edit">
+            {" "}
+            <LuFileEdit />{" "}
+          </button>
+
+          <button
+            className="text-red-700 ml-1"
+            title="Delete"
+            onClick={() => deleteCategory.mutate(category.id)}
+          >
+            {" "}
+            <FaTrashAlt />{" "}
+          </button>
+        </div>
+      </td>
+    </tr>
+  );
+};
+
+export default Category;
